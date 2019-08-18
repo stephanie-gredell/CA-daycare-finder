421c57992ff821485575c5e9db1923aeb6d7618e
Stephanie Chung <s.raide@gmail.com>
2019-08-18 01:12:14 -0700

    Updates

---
+++ precache-manifest.f89f132958a8aed27efde0d54492b767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613f4fad566a7bd72ab66ae300c591ac",
+    "url": "/CA-daycare-finder/index.html"
+  },
+  {
+    "revision": "660e8c24b588c0495405",
+    "url": "/CA-daycare-finder/static/css/main.a0c76b94.chunk.css"
+  },
+  {
+    "revision": "6c3ed10ffb2a877cbf4e",
+    "url": "/CA-daycare-finder/static/js/2.1c69b7d8.chunk.js"
+  },
+  {
+    "revision": "660e8c24b588c0495405",
+    "url": "/CA-daycare-finder/static/js/main.30b6acf1.chunk.js"
+  },
+  {
+    "revision": "1118c375d268db6fe95f",
+    "url": "/CA-daycare-finder/static/js/runtime~main.dcbab462.js"
+  }
+]);
